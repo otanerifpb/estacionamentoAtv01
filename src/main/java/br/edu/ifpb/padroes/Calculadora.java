1990b620b4c0bdd530b610ca5b1e766f970ee3d1
otanerifpb <renato.silva.pro@gmail.com>
2022-02-26 14:22:21 -0300

    Update Calculadora.java

--- src/main/java/br/edu/ifpb/padroes/Calculadora.java
+++ src/main/java/br/edu/ifpb/padroes/Calculadora.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Calculadora {
-    //public Long calcularDuracao(LocalDateTime saida, LocalDateTime entrada){
+
     public BigDecimal calcularDuracao(LocalDateTime saida, LocalDateTime entrada) {
         long periodoHoras = Duration.between(saida, entrada).toHours();
         long periodoDias = Duration.between(saida, entrada).toDays();
@@ -23,26 +23,6 @@ public class Calculadora {
             valor = VALOR_MENSALIDADE.valor;
     }
         return valor;
-
-        //valor = VALOR_HORA.xmultiply(BigDecimal.valueOf(calculadora.calcularDuracao(entrada, saida)));
-
-        //long diferenca = Duration.between(entrada, saida).toDays();
-
-        //        if (periodoHoras < 12) {
-//            valor = VALOR_HORA.multiply(new BigDecimal(periodoHoras));
-//        } else if (periodoHoras > 12 && periodoDias < 15) {
-//            valor = VALOR_DIARIA.multiply(new BigDecimal(periodoDias));
-//        } else if (periodoDias > 15) {
-//            valor = VALOR_MENSALIDADE;
-//        }
-
-//        if (periodoHoras < 12) {
-//            return  duracao = periodoHoras;
-//        } else if (periodoHoras > 12 && periodoDias < 15) {
-//            return duracao = periodoDias;
-//        } else  {
-//            return duracao = periodoMes;
-//        }
 
     }
 }
